fc395e69645249a029d9ac120ae6f7c2e23b1be5
Zsolt Dollenstein <zsol@starcraft2.hu>
2011-02-02 22:11:44 +0000

    more minor customizations

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/symfony/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__).'/../lib/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
