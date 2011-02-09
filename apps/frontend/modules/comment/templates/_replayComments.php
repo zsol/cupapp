215dd12ce95f3420932dc4333888f4fb44bca896
Zsolt Dollenstein <zsol@starcraft2.hu>
2011-02-09 15:19:00 +0000

    fix some comment stuff

--- apps/frontend/modules/comment/templates/_replayComments.php
+++ apps/frontend/modules/comment/templates/_replayComments.php
@@ -1,11 +1,18 @@
 <?php if (count($comments)) : ?>
+    <?php use_javascript('comments.js') ?>
     <?php $i = 1; ?>
     <?php foreach ($comments as $comment) : ?>
         <?php $user = $comment->getSfGuardUser() ?>
         <div class="comment" id="comment_<?php echo $comment->getId() ?>">
             <div class="comment_header">
-                <a name="comment_<?php echo $comment->getId() ?>" href="#comment_<?php echo $comment->getId() ?>">#<?php echo $i ?></a> <?php echo $user->getUsername()  ?> - Reply - Delete
-                <span style="float:right;"><?php echo $comment->getCreatedAt() ?></span>
+                <a name="comment_<?php echo $comment->getId() ?>" href="#comment_<?php echo $comment->getId() ?>">#<?php echo $i ?></a> <?php echo $user->getUsername()  ?>
+	   <?php if ($sf_user->isAuthenticated() && $sf_user->isAllowedToComment()):?>
+  	     - <a href="#comment_form" onclick="replyTo(<?php echo $comment->getId()?>)"><?php echo __("Reply")?></a>
+	     <?php if ($sf_user->isSuperAdmin()): ?>
+	       - Delete
+    	     <?php endif; ?>
+	   <?php endif; ?>
+	   <span style="float:right;"><?php echo $comment->getCreatedAt() ?></span>
             </div>
             <div class="comment_body">
                 <div class="comment_avatar"><img style="float:left;" alt="avatar" src="<?php echo $user->getProfile()->getAvatarOrDefaultUrl() ?>"/></div>
