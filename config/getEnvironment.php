466a6340f817a2d2bfbbccb9eeed03e1a5ca1906
Zsolt Dollenstein <zsol@starcraft2.hu>
2011-02-11 18:20:40 +0000

    hopefully final hostname configs

--- config/getEnvironment.php
+++ config/getEnvironment.php
@@ -1,10 +1,10 @@
 <?php
 
-if ($_SERVER['SERVER_NAME'] == 'cupapp.dyndns.info') {
+if ($_SERVER['SERVER_NAME'] == 'test.cupapp.com') {
     $env = 'dev';
     $debug = true;
 }
-else if ($_SERVER['SERVER_NAME'] == 'my.wild.dre.am') {
+else if ($_SERVER['SERVER_NAME'] == 'www.cupapp.com' || $_SERVER['SERVER_NAME'] == 'cupapp.com') {
     $env = 'prod';
     $debug = false;
 }
