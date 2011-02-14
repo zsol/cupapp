7dd2a02d1eb2552bcedb11f8e327543beb3b00b0
Zsolt Dollenstein <zsol@starcraft2.hu>
2011-02-14 20:33:16 +0000

    minimalistic terms&conditions page take2

---
+++ apps/frontend/modules/rules/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * rules actions.
+ *
+ * @package    Cupapp
+ * @subpackage rules
+ * @author     Your name here
+ * @version    SVN: $Id$
+ */
+class rulesActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
---
+++ apps/frontend/modules/rules/templates/indexSuccess.php
@@ -0,0 +1,18 @@
+<?php slot('title'); ?>
+   <?php echo __('Terms and Conditions'); ?>
+<?php end_slot(); ?>
+
+<?php slot('leftbar'); ?>
+    <?php include_partial('global/leftbar') ?>
+<?php end_slot(); ?>
+
+ <?php slot('rightbar'); ?>
+    <?php include_partial('global/rightbar') ?>
+<?php end_slot(); ?>
+
+<div class="greybox">
+  <h1 id="rules_title"><?php echo __('Terms and Conditions');?></h1>
+  <?php echo __('This is a really simple T&C for the sake of human readability. By using the site, you agree to, first and foremost, act in good belief; use the site as it was intended to be used (share replays); not be a troll and be nice to other users of the site.'); ?>
+  <hr />
+  <?php echo __('If you see someone you think is not adhering to above rules, do not hesitate to %%s%%.', array('%%s%%' => '<a href="' . url_for('replay/contact') . '">' . __('contact the admins') . '</a>')); ?>
+</div>
