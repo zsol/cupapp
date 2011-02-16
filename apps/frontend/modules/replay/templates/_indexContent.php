f817357b9b884f09d8ffede8e48758b62f9c06e2
Anathien <anathien@gmail.com>
2011-02-16 22:31:26 +0000

    Changed font type and size on the overall page. Formatted index text. Moved style definition from footer into css.

--- apps/frontend/modules/replay/templates/_indexContent.php
+++ apps/frontend/modules/replay/templates/_indexContent.php
@@ -1,6 +1,11 @@
-<div style="padding:10px;font-size:20px;">
-   <p><?php echo __("Hello dear visitor! What you see now are the beginnings of a replay sharing site. Please use it to your pleasure! If you find any bugs, sending the details to my email address (%%contact%%) would help a lot. Thanks in advance.", array('%%contact%%' => link_to(__("Contact"), 'replay/contact'))); ?>
-</p>
-<p>Zsol</p>
-<p><i><?php echo __("Ps: Don't store your replays here, as I might have to wipe the database every now and then."); ?></i></p>
+<div id="index_div">
+	<p>
+		Hello dear visitor! <br/><br/>
+	   	<?php echo __("What you see now are the beginnings of a replay sharing site. Please use it to your pleasure! If you find any bugs, sending the details to my email address (%%contact%%) would help a lot.", array('%%contact%%' => link_to(__("Contact"), 'replay/contact'))); ?> <br/><br/>
+	   	Thanks in advance. <br/>
+		Zsol <br/><br/>
+	</p>
+	<p>
+		<i><?php echo __("Ps: Don't store your replays here, as I might have to wipe the database every now and then."); ?></i>
+	</p>
 </div>
