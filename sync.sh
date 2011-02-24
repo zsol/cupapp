fe156193a8c920b66fb876db413688ffa127dc28
Zsolt Dollenstein <zsol@starcraft2.hu>
2011-02-24 07:38:03 +0000

    sync script, suitable to be run from cron

---
+++ sync.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BASEDIR=${BASEDIR:-$(dirname $(readlink -f $0))}
+BRANCH=${BRANCH:-"master"}
+pushd "$BASEDIR" > /dev/null
+
+function needs_sync_with()
+{
+    if [ "$(git ls-remote --heads $1 $BRANCH)" == "$(git ls-remote --heads ./. $BRANCH)" ]; then
+	return 1
+    fi
+    return 0
+}
+
+if needs_sync_with "origin" ; then
+    cmd="git pull -q --ff-only -- origin $BRANCH"
+    echo "Syncing (running $cmd)"
+    eval "$cmd" || exit 2
+fi
+
+popd > /dev/null
+
