b336b5041c5b3fa257f9087438fe3dd66429c72a
Zsolt Dollenstein <zsol@starcraft2.hu>
2011-03-01 20:53:50 +0000

    add more credit to ppl who deserve it :P

--- apps/frontend/modules/replay/templates/_contactContent.php
+++ apps/frontend/modules/replay/templates/_contactContent.php
@@ -1,1 +1,10 @@
-zsol at starcraft2 [] hu
+<div>
+<div style="float:left">
+zsol <br />
+anathien <br />
+aston
+</div>
+<div style="vertical-align: middle; line-height:3.7em">
+@starcraft2.hu
+</div>
+</div>
