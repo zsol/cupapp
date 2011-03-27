779f2c4e1a466e29c0b01ca6811d6bb8faabb7c8
Aston <agoston.fung@gmail.com>
2011-03-27 20:44:09 +0100

    Adding meta tags to layouts and translation. Added another email to contact page.

---
+++ apps/frontend/templates/_metas.php
@@ -0,0 +1,3 @@
+<meta name="description" content="<?php echo __('layout.metas.description') ?>"/>
+<meta name="keywords" content="<?php echo __('layout.metas.keywords') ?>"/>
+<meta name="language" content="<?php echo $sf_user->getCulture() ?>"/>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $sf_user->getCulture() ?>" lang="<?php echo $sf_user->getCulture() ?>">
   <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
+    <?php include_partial('global/metas') ?>
     <title>CupApp<?php if (has_slot('title')) { echo " - "; include_slot('title'); } ?></title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
