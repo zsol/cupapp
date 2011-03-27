b8ef6d6e64dba23475c6b35bc2d6e4ed32c456ff
Agoston <agoston.fung@gmail.com>
2011-03-27 13:55:37 -0700

    Edited apps/frontend/modules/cms/templates/homeSuccess.php via GitHub

--- apps/frontend/modules/cms/templates/homeSuccess.php
+++ apps/frontend/modules/cms/templates/homeSuccess.php
@@ -15,7 +15,7 @@
     <div id="index_div">
             <p>
                     Hello dear visitor! <br/><br/>
-                    <?php echo __("What you see now are the beginnings of a replay sharing site. Please use it to your pleasure! If you find any bugs, sending the details to my email address (%%contact%%) would help a lot.", array('%%contact%%' => link_to(__("Contact"), 'replay/contact'))); ?> <br/><br/>
+                    <?php echo __("What you see now are the beginnings of a replay sharing site. Please use it to your pleasure! If you find any bugs, sending the details to my email address (%%contact%%) would help a lot.", array('%%contact%%' => link_to(__("Contact"), '@contact'))); ?> <br/><br/>
                     <?php echo __("Thanks in advance.")?> <br/>
                     Zsol <br/><br/>
             </p>
