6b82cc1509d61f6b315ebe94e74aff233e006075
Zsolt Dollenstein <zsol@starcraft2.hu>
2011-06-13 12:18:54 +0100

    add update message to homepage

--- apps/frontend/modules/cms/templates/homeSuccess.php
+++ apps/frontend/modules/cms/templates/homeSuccess.php
@@ -14,10 +14,13 @@
     <h1 id="replay_index_title"><?php echo __('Home') ?></h1>
     <div id="index_div">
             <p>
-                    Hello dear visitor! <br/><br/>
-                    <?php echo __("What you see now are the beginnings of a replay sharing site. Please use it to your pleasure! If you find any bugs, sending the details to my email address (%%contact%%) would help a lot.", array('%%contact%%' => link_to(__("Contact"), '@contact'))); ?> <br/><br/>
-                    <?php echo __("Thanks in advance.")?> <br/>
-                    Zsol <br/><br/>
+              <?php echo __("Hello dear visitor!") ?> <br/><br/>
+              <?php echo __("What you see now are the beginnings of a replay sharing site. Please use it to your pleasure! If you find any bugs, sending the details to my email address (%%contact%%) would help a lot.", array('%%contact%%' => link_to(__("Contact"), '@contact'))); ?> <br/><br/>
+              <?php echo __("Thanks in advance.")?> <br/>
+              <hr />
+              <span class="home_update"><?php echo __("Update:") ?></span>
+              <?php echo __("You now can connect your facebook account to the site! If you already have a username and password here, just log in with those and then click on the facebook connect button. Once this is done, you will always be logged in if you are logged in to facebook.") ?> <br/>
+              <?php echo __("If you don't have an account on this site yet, you can create one by clicking on %%REGISTER%%, and then choosing the facebook button. This will automatically connect with your facebook account and you won't have to remember any passwords", array('%%REGISTER%%' => __("Register"))); ?> <br/>
             </p>
     </div>
 <div style="clear:both;"></div>
